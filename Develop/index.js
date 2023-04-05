1d152d2639b1a515c45919192b662af89121d85d
novasm55 <novasmichael93@gmail.com>
2023-04-04 21:09:27 -0700

    Final Project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+    {
+        name: 'fileName',
+        type: 'input',
+        message: 'Enter fileName: ',
+      
+    },
+    {
+        name: 'projectTitle',
+        type: 'input',
+        message: 'Enter projectTitle: ',
+      
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Enter projectDescription: ',
+    
+    },
+
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Enter installation instructions:',
+     
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Enter usage information:',
+        
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Project license:',
+        choices: ['MIT', 'GPL', 'Apache', 'BSD', 'None']
+    },
+    {
+        name: 'contributing',
+        type: 'input',
+        message: 'OpenSource contribution needs: ',
+      
+    },
+
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Enter testing information:',
+       
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Email:',
+
+    },
+    {
+        name: 'username',
+        type: 'input',
+        message: 'GitHub username:',
+   
+    },
+
+    
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, function (e) {
+    e ? console.log(e) : console.log("File " + fileName + " generated successfully!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers)=>{
+            const md = generateMarkdown(answers);
+            writeToFile(`${answers.fileName}.md`, md);
+        })
+        .catch((e) => console.error(e));
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license) {
+    return '';
+  }
+  else return `![License](https://img.shields.io/badge/license-${license}-red.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license){
+    return '';
+  }
+  else return '\n* [License](#license)\n'
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return '';
+  }
+  else return `
+  ${renderLicenseBadge(license)}
+
+  This project was created under the ${license} license and ${data.username} possesses all the rights and privileges this license confers on its possessor!`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
-`;
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Description 
+
+${data.description}
+
+## Installation
+
+Installation instructions:
+
+${data.installation}
+
+## Usage
+
+Usage instructions:
+
+${data.usage}
+
+## License
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+Test Instructions:
+
+${data.tests}
+
+## Questions
+
+If you have questions, suggestions, comments or concerns please reach me at ${data.email}. 
+
+Check out my portfolio at ${data.username}.`;
 }
 
 module.exports = generateMarkdown;
